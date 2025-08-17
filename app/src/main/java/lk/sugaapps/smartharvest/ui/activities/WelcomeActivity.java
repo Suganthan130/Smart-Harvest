ce5e0bb9fe08f8c76ba955a277ecb3794db4007d
Suganthan <kumaralingam@unionassurance.com>
2025-08-17 19:23:22 +0530

    Refactor: Simplify click listeners in WelcomeActivity
    This commit updates the `setOnClick()` method in `WelcomeActivity.java`:
    
    - The click listeners for `btnSignIn` and `btnSignUp` have been refactored to use lambda expressions for brevity.
    - The `android.view.View` import is no longer needed and has been removed.

--- app/src/main/java/lk/sugaapps/smartharvest/ui/activities/WelcomeActivity.java
+++ app/src/main/java/lk/sugaapps/smartharvest/ui/activities/WelcomeActivity.java
@@ -2,7 +2,6 @@ package lk.sugaapps.smartharvest.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,17 +21,9 @@ public class WelcomeActivity extends AppCompatActivity {
 
     }
     private void setOnClick() {
-        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(WelcomeActivity.this,LoginActivity.class));
-            }
-        });
-        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(WelcomeActivity.this,RegisterActivity.class));
-            }
-        });
+
+        binding.btnSignIn.setOnClickListener(v -> startActivity(new Intent(WelcomeActivity.this,LoginActivity.class)));
+
+        binding.btnSignUp.setOnClickListener(v -> startActivity(new Intent(WelcomeActivity.this,RegisterActivity.class)));
     }
 }
