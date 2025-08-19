f2365eff6158356af4e122cd8e7486791b4436c9
Suganthan <kumaralingam@unionassurance.com>
2025-08-19 19:39:41 +0530

    Refactor: Relocate shimmer component classes
    This commit moves the shimmer-related classes to a new package.
    
    - The following classes have been moved from `lk.sugaapps.smartharvest.ui.components.shimmer` to `lk.sugaapps.smartharvest.ui.view.shimmer`:
        - `ShimmerDrawable.java`
        - `ShimmerFrameLayout.java`
        - `Shimmer.java`
    - The package declaration in each of these files has been updated to reflect the new location.

--- app/src/main/java/lk/sugaapps/smartharvest/ui/view/shimmer/ShimmerFrameLayout.java
+++ app/src/main/java/lk/sugaapps/smartharvest/ui/view/shimmer/ShimmerFrameLayout.java
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package lk.sugaapps.smartharvest.ui.components.shimmer;
+package lk.sugaapps.smartharvest.ui.view.shimmer;
 
 import android.content.Context;
 import android.content.res.TypedArray;
